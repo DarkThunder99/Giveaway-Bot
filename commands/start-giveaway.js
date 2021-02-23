2210f32bb955f1875db2b0c033ca5d824edf974b
DarkThunder99 <75782012+DarkThunder99@users.noreply.github.com>
2021-02-23 14:56:05 +0530

    Update start-giveaway.js

--- commands/start-giveaway.js
+++ commands/start-giveaway.js
@@ -2,50 +2,35 @@ const ms = require('ms');
 
 exports.run = async (client, message, args) => {
 
-    // If the member doesn't have enough permissions
     if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
         return message.channel.send(':x: You need to have the manage messages permissions to start giveaways.');
     }
 
-    // Giveaway channel
     let giveawayChannel = message.mentions.channels.first();
-    // If no channel is mentionned
     if(!giveawayChannel){
         return message.channel.send(':x: You have to mention a valid channel!');
     }
 
-    // Giveaway duration
     let giveawayDuration = args[1];
-    // If the duration isn't valid
     if(!giveawayDuration || isNaN(ms(giveawayDuration))){
         return message.channel.send(':x: You have to specify a valid duration!');
     }
 
-    // Number of winners
     let giveawayNumberWinners = args[2];
-    // If the specified number of winners is not a number
     if(isNaN(giveawayNumberWinners) || (parseInt(giveawayNumberWinners) <= 0)){
         return message.channel.send(':x: You have to specify a valid number of winners!');
     }
 
-    // Giveaway prize
     let giveawayPrize = args.slice(3).join(' ');
-    // If no prize is specified
     if(!giveawayPrize){
         return message.channel.send(':x: You have to specify a valid prize!');
     }
 
-    // Start the giveaway
     client.giveawaysManager.start(giveawayChannel, {
-        // The giveaway duration
         time: ms(giveawayDuration),
-        // The giveaway prize
         prize: giveawayPrize,
-        // The giveaway winner count
         winnerCount: giveawayNumberWinners,
-        // Who hosts this giveaway
         hostedBy: client.config.hostedBy ? message.author : null,
-        // Messages
         messages: {
             giveaway: (client.config.everyoneMention ? "@everyone\n\n" : "")+"🎉🎉 **GIVEAWAY** 🎉🎉",
             giveawayEnded: (client.config.everyoneMention ? "@everyone\n\n" : "")+"🎉🎉 **GIVEAWAY ENDED** 🎉🎉",
@@ -62,7 +47,7 @@ exports.run = async (client, message, args) => {
                 minutes: "minutes",
                 hours: "hours",
                 days: "days",
-                pluralS: false // Not needed, because units end with a S so it will automatically removed if the unit value is lower than 2
+                pluralS: false
             }
         }
     });
