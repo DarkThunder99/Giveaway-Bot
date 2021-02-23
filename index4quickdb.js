4d48799a88536b8b5baed18a63349dc6c53e0dd8
DarkThunder99 <75782012+DarkThunder99@users.noreply.github.com>
2021-02-23 14:52:43 +0530

    Update index4quickdb.js

--- index4quickdb.js
+++ index4quickdb.js
@@ -11,47 +11,29 @@ const db = require("quick.db");
 if(!db.get("giveaways")) db.set("giveaways", []);
 
 const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
-
-    // This function is called when the manager needs to get all the giveaway stored in the database.
     async getAllGiveaways(){
-        // Get all the giveaway in the database
         return db.get("giveaways");
     }
 
-    // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
     async saveGiveaway(messageID, giveawayData){
-        // Add the new one
         db.push("giveaways", giveawayData);
-        // Don't forget to return something!
         return true;
     }
 
     async editGiveaway(messageID, giveawayData){
-        // Gets all the current giveaways
         const giveaways = db.get("giveaways");
-        // Remove the old giveaway from the current giveaways ID
         const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
-        // Push the new giveaway to the array
         newGiveawaysArray.push(giveawayData);
-        // Save the updated array
         db.set("giveaways", newGiveawaysArray);
-        // Don't forget to return something!
         return true;
     }
-
-    // This function is called when a giveaway needs to be deleted from the database.
     async deleteGiveaway(messageID){
-        // Remove the giveaway from the array
         const newGiveawaysArray = db.get("giveaways").filter((giveaway) => giveaway.messageID !== messageID);
-        // Save the updated array
         db.set("giveaways", newGiveawaysArray);
-        // Don't forget to return something!
         return true;
     }
 
 };
-
-// Create a new instance of your new class
 const manager = new GiveawayManagerWithOwnDatabase(client, {
     storage: false,
     updateCountdownEvery: 5000,
@@ -63,7 +45,6 @@ const manager = new GiveawayManagerWithOwnDatabase(client, {
     }
 });
 client.giveawaysManager = manager;
-// We now have a client.giveawaysManager property to manage our giveaways!
 
 client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
     console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
@@ -73,7 +54,6 @@ client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reactio
     console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
 });
 
-/* Load all events */
 fs.readdir("./events/", (_err, files) => {
     files.forEach((file) => {
         if (!file.endsWith(".js")) return;
@@ -87,7 +67,6 @@ fs.readdir("./events/", (_err, files) => {
 
 client.commands = new Discord.Collection();
 
-/* Load all commands */
 fs.readdir("./commands/", (_err, files) => {
     files.forEach((file) => {
         if (!file.endsWith(".js")) return;
@@ -98,5 +77,4 @@ fs.readdir("./commands/", (_err, files) => {
     });
 });
 
-// Login
 client.login(config.token);
